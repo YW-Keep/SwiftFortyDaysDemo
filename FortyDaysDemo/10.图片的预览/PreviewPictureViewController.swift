ba8d7594214c91e229c66264e56a8fb8667211dc
tangyuwei <tangyuwei@myhexin.com>
2017-11-14 21:19:59 +0800

    优化图片预览代码

--- FortyDaysDemo/10.图片的预览/PreviewPictureViewController.swift
+++ FortyDaysDemo/10.图片的预览/PreviewPictureViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 
 class PreviewPictureViewController: UIViewController,UIScrollViewDelegate,UIGestureRecognizerDelegate {
-    var  multiple: CGFloat = 1
     
-    var isFirst: Bool = true
-    var rotationRem: CGFloat = 0
-    var point: CGPoint = CGPoint(x: 0, y: 0)
+    // 记录所有变化方式
+    private var rotateTransform = CGAffineTransform(rotationAngle: 0)
+    private var pinchTransform = CGAffineTransform(scaleX: 1,y: 1)
+    private var panTransform = CGAffineTransform(translationX: 0, y: 0)
+    private var gesturesArray: [UIGestureRecognizer]!
+    
     lazy var imageView: UIImageView = {
         let imageView = UIImageView(frame: CGRect(x: 0, y: (kScreenHeight - 300 - 64)/2.0, width: kScreenWidth, height: 300))
         imageView.image = #imageLiteral(resourceName: "gastronomy2.jpg")
@@ -39,17 +41,8 @@ class PreviewPictureViewController: UIViewController,UIScrollViewDelegate,UIGest
         
         imageView.isUserInteractionEnabled = true
         // 这样设置会有个小问题，一些设置的值会存在相互影响的可能性，这里就不再深入探究了，如果要效果好，用单一手势 然后做单一transform 修改 效果肯定杠杠的
+        gesturesArray = [rotateGesture, pinchGesture, panGesture]
         return imageView ;
-    }()
-    
-    lazy var scrollView: UIScrollView = {
-        let scroll = UIScrollView(frame: CGRect(x: 0, y: 64, width: kScreenWidth, height: kScreenHeight - 64))
-        scroll.addSubview(imageView)
-        scroll.backgroundColor  = UIColor.yellow
-        scroll.maximumZoomScale = 3.0
-        scroll.minimumZoomScale = 0.5
-        scroll.delegate = self
-        return scroll
     }()
     
     lazy var backImageView: UIImageView = {
@@ -81,65 +74,56 @@ class PreviewPictureViewController: UIViewController,UIScrollViewDelegate,UIGest
     
     //旋转手势
     @objc func rotateGestureAction(sender: UIRotationGestureRecognizer) {
-        //浮点类型，得到sender的旋转度数
-        let rotation : CGFloat =  sender.rotation - rotationRem
-        rotationRem = sender.rotation
-        //旋转角度CGAffineTransformMakeRotation,改变图像角度
-        imageView.transform = imageView.transform.rotated(by:rotation)
+        rotateTransform = CGAffineTransform(rotationAngle: sender.rotation)
+        imageView.transform = mergeTransform()
         //状态结束 返回原样
         if sender.state == UIGestureRecognizerState.ended{
-            imageView.transform = CGAffineTransform(rotationAngle: 0)
-            imageView.frame = CGRect(x: 0, y: (kScreenHeight - 300 - 64)/2.0, width: kScreenWidth, height: 300)
+            endClean()
         }
     }
     // 捏合手势
     @objc func pinchGestureAciton(sender: UIPinchGestureRecognizer) {
-       
-        let factor = sender.scale/multiple
-         multiple = sender.scale
-        imageView.transform = imageView.transform.scaledBy(x: factor, y: factor)
-       
+        pinchTransform = CGAffineTransform(scaleX: sender.scale,y: sender.scale)
+        imageView.transform = mergeTransform()
         //状态是否结束，如果结束保存数据
         if sender.state == UIGestureRecognizerState.ended{
-            imageView.transform = CGAffineTransform(scaleX: 1,y: 1)
-            imageView.frame = CGRect(x: 0, y: (kScreenHeight - 300 - 64)/2.0, width: kScreenWidth, height: 300)
+            endClean()
         }
     }
     //拖手势
     @objc func panGestureAction(sender: UIPanGestureRecognizer){
         //得到拖的过程中的xy坐标
         let translation : CGPoint = sender.translation(in: imageView)
-        //平移图片CGAffineTransformMakeTranslation
-        let ponitX = translation.x - point.x
-        let pointY = translation.y - point.y
-        point = translation
-        imageView.transform = imageView.transform.translatedBy(x: ponitX, y: pointY)
-//        imageView.transform.concatenating(<#T##t2: CGAffineTransform##CGAffineTransform#>)
+        panTransform = CGAffineTransform(translationX: translation.x, y: translation.y)
+        imageView.transform = mergeTransform()
         if sender.state == UIGestureRecognizerState.ended{
-            imageView.transform = CGAffineTransform(translationX: 0, y: 0)
-            imageView.frame = CGRect(x: 0, y: (kScreenHeight - 300 - 64)/2.0, width: kScreenWidth, height: 300)
+            endClean()
         }
+    }
+    
+    // 合并手势
+    func mergeTransform() -> CGAffineTransform {
+        var transform = pinchTransform
+        transform = transform.concatenating(rotateTransform)
+        transform = transform.concatenating(panTransform)
+        return transform
+    }
+    
+    // 清空变为原样
+    func endClean() {
+        imageView.transform = CGAffineTransform(translationX: 0, y: 0)
+        imageView.frame = CGRect(x: 0, y: (kScreenHeight - 300 - 64)/2.0, width: kScreenWidth, height: 300)
+        rotateTransform = CGAffineTransform(rotationAngle: 0)
+        pinchTransform = CGAffineTransform(scaleX: 1,y: 1)
+        panTransform = CGAffineTransform(translationX: 0, y: 0)
     }
     
     //MARK: - UIGestureRecognizerDelegate
     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        return true
-    }
-
-    
-    //MARK: - UIScrollViewDelegate
-    func scrollViewDidZoom(_ scrollView: UIScrollView) {
-//        print(scrollView.zoomScale)
-        // 居中显示
-        let offsetX = (scrollView.bounds.size.width > scrollView.contentSize.width) ? (scrollView.bounds.size.width - scrollView.contentSize.width) * 0.5 : 0.0;
-        let offsetY = (scrollView.bounds.size.height > scrollView.contentSize.height) ?
-            (scrollView.bounds.size.height - scrollView.contentSize.height) * 0.5 : 0.0;
-        imageView.center = CGPoint(x: scrollView.contentSize.width * 0.5 + offsetX, y: scrollView.contentSize.height * 0.5 + offsetY)
-//        imageView.frame.size.height = 300 * scrollView.zoomScale
-//        imageView.frame.size.width = kScreenWidth * scrollView.zoomScale
+        if gesturesArray.contains(gestureRecognizer) && gesturesArray.contains(otherGestureRecognizer){
+            return true
+        }
+        return false
     }
     
-    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return imageView
-    }
 }
